09443484822857113bc388c4d8b30d569380a303
danudnas <danudnas@hotmail.com>
2022-01-21 11:16:29 +0000

    Initial commit

---
+++ src/main/java/tml/centralapi/operatormanager/OperatormanagerApplication.java
@@ -0,0 +1,13 @@
+package tml.centralapi.operatormanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatormanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatormanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tml/centralapi/operatormanager/OperatormanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tml.centralapi.operatormanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatormanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
